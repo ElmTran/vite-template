6768125d369aa21d840331ff2b20543f947f7f5c
Mengqing <c897611977@gmail.com>
2023-04-23 15:31:35 +0100

    add iconfont

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import SvgIcon from '@/components/SvgIcon/index.vue' // svg component
 
-import './assets/main.css'
+import '@/assets/main.css'
+import 'element-plus/dist/index.css'
+import '@/assets/iconfont/index.js'
+import '@/assets/iconfont/font.css'
 
 const app = createApp(App)
+app.component('SvgIcon', SvgIcon) // register globally
 
 app.use(createPinia())
 app.use(router)
