e1d64fb8ca25519e945e7b65ec63f571d488fb32
Mengqing <c897611977@gmail.com>
2023-04-23 15:35:53 +0100

    add component/SvgIcon

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <svg :class="svgClass" aria-hidden="true">
+        <use :xlink:href="svgIcon" />
+    </svg>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue'
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    class: {
+        type: String,
+        default: '',
+    },
+})
+
+const svgClass = computed(() => {
+    if (props.class) {
+        return `svg-icon ${props.class}`
+    } else {
+        return 'svg-icon'
+    }
+})
+
+const svgIcon = computed(() => `#${props.name}`)
+
+</script>
+
+<style lang="scss" scoped>
+.svg-icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
+</style>
